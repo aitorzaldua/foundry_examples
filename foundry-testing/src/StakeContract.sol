519ebaa515b82dab5abe179abee753d40295e95f
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-03-14 20:04:50 +0100

    foundry-testing starting contract

--- foundry-testing/src/StakeContract.sol
+++ foundry-testing/src/StakeContract.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract StakeContract {
-    
+
+    mapping (uint => mapping (address => uint)) s_balances;
+
+    function stake(address token, uint amount) external returns(bool){
+        s_balances[msg.sender][token] += amount;
+        // @comment - como funciona IERC20 de OZ?
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
+
+    }
 }
