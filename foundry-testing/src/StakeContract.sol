66d831e15822d63c858327e7b17c531df1ba9f7c
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-03-14 20:28:42 +0100

    staking contract, starting tests

--- foundry-testing/src/StakeContract.sol
+++ foundry-testing/src/StakeContract.sol
@@ -3,14 +3,19 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+error StakeContract_TransferFailed();
+
 contract StakeContract {
 
-    mapping (uint => mapping (address => uint)) s_balances;
+    mapping (address => uint256) s_balances;
 
     function stake(address token, uint amount) external returns(bool){
-        s_balances[msg.sender][token] += amount;
+        s_balances[msg.sender] = s_balances[msg.sender] + amount;
         // @comment - como funciona IERC20 de OZ?
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol
+        bool success = IERC20(token).transferFrom(msg.sender, address(this), amount);
+        if (!success) revert StakeContract_TransferFailed();
+        return success;
 
     }
 }
---
+++ foundry-testing/test/StakeContract.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/StakeContract.sol";
+
+contract CounterTest is Test {
+    function setUp() public {
+    }
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
